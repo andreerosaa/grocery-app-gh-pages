8f08c85f8a555b5c6efa8d4c6fad2926cc464d9b
André Santos Rosa <arossant@emeal.nttdata.com>
2023-03-16 10:36:30 +0000

    finnished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
@@ -9,6 +10,8 @@ function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
 
   const [newItem, setNewItem] = useState('');
+
+  const [search, setSearch] = useState('');
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
@@ -48,8 +51,11 @@ function App() {
         newItem = {newItem}
         setNewItem = {setNewItem}
         handleSubmit = {handleSubmit}/>
+      <SearchItem
+        search = {search}
+        setSearch = {setSearch}/>
       <Content
-        items = {items}
+        items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck = {handleCheck}
         handleDelete = {handleDelete}/>
       <Footer length={items.length}/>
--- src/SearchItem.js
+++ src/SearchItem.js
@@ -1,8 +1,15 @@
-import React from 'react'
-
-const SearchItem = () => {
+const SearchItem = ({search, setSearch}) => {
   return (
-    <div>SearchItem</div>
+    <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor='search'>Search</label>
+      <input
+        id='search'
+        type='text'
+        role='searchbox'
+        placeholder='Search Items'
+        value = {search}
+        onChange = {(e) => setSearch(e.target.value)}/>
+    </form>
   )
 }
 
